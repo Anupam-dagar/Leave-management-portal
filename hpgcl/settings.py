c646f26cd2681cc3155b26d2cbe61a763e0b36bc
Anupam-dagar <dagaranupam@gmail.com>
2018-07-24 22:57:19 +0530

    add to nav bar

--- hpgcl/settings.py
+++ hpgcl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+p279_e$)b00m*onbejdvem+(b#@rpcdb&o=-5t2ixsc39z+9s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'leave.User'
 
