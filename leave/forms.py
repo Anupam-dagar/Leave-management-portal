d25b6c412572509a8ad3ba06e31d3935ca6c3de7
Anupam Dagar <dagaranupam@gmail.com>
2018-12-24 00:56:50 +0530

    fix server error due to forms.py. Add a new field noted_to, rename older one to noted_by.

--- leave/forms.py
+++ leave/forms.py
@@ -8,15 +8,19 @@ class MyCustomUserForm(RegistrationForm):
         exclude =  ['available_leaves', 'last_login', 'is_superuser', 'groups', 'user_permissions', 'is_staff', 'is_active', 'date_joined_0', 'date_joined_1', 'date_joined', 'password']
 
 class LeaveForm(forms.ModelForm):
-    def __init__(self, userdetails, *args, **kwargs):
+    def __init__(self, userdetails, request_method, *args, **kwargs):
         super (LeaveForm,self).__init__(*args,**kwargs)
-        if userdetails.designation == 'SE':
-            self.fields['noted_to'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='CE')
-        if userdetails.designation == 'XEN':
-            self.fields['noted_to'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='SE')
-        if userdetails.designation == 'AEE':
-            self.fields['noted_to'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='XEN')
+        if request_method == 'GET':
+            if userdetails.designation == 'SE':
+                self.fields['noted_by'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='CE')
+                self.fields['noted_to'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='SE')
+            if userdetails.designation == 'XEN':
+                self.fields['noted_by'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='SE')
+                self.fields['noted_to'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='XEN')
+            if userdetails.designation == 'AEE':
+                self.fields['noted_by'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='XEN')
+                self.fields['noted_to'].queryset = User.objects.filter(power_station=userdetails.power_station, designation='AEE')
 
     class Meta:
         model = Leave
-        fields = ('noted_to', 'from_date', 'to_date', 'purpose')
+        fields = ('noted_by', 'from_date', 'to_date', 'purpose', 'noted_to')
--- leave/models.py
+++ leave/models.py
@@ -37,11 +37,12 @@ class User(AbstractUser):
 class Leave(models.Model):
     employee = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
     sanctioned_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_sanctioned', blank=True, null=True)
-    noted_to =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_noted', blank=False, null=False)
+    noted_by =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_notedby', blank=False, null=False)
     from_date = models.DateField(blank=False, null=False)
     to_date = models.DateField(blank=False, null=False)
     purpose = models.CharField(max_length=1200, blank=False, null=False)
     approval = models.BooleanField(default=False, blank=False, null=False)
+    noted_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_notedto', blank=True, null=True)
 
     def __str__(self):
         return self.employee.first_name + ': ' + str(self.from_date) + ' - ' + str(self.to_date)
